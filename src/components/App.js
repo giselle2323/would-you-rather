18a0c3411e0b094659b52ca2784ac82cde854739
giselle2323 <yakubuaminat94@gmail.com>
2020-08-21 00:40:34 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ROUTES, {RenderRoutes} from '../routes/routes'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <h1>App</h1>
+
+      <div>
+        <RenderRoutes routes={ROUTES} />
+      </div>
+    </section>
+  );
+}
+
+export default App;
