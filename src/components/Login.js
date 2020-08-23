d85b710706eb6bb71a13fdd44b0eddac721ac8a1
giselle2323 <yakubuaminat94@gmail.com>
2020-08-23 23:52:23 +0100

    get users logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Select from 'react-select'
+import {_getUsers} from '../utils/_DATA'
 
 
 const Login = () => {
+
+  useEffect(() => {
+    
   
+  }, []);
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
  return (
   <>
      <div>Login page</div>
      <form>
 
-
-
+       <label for="userSelect">Select User </label>
+       <Select options={options} />
+      <input type="submit" value='Submit'/>
      </form>
   </>
  )
