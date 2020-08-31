f1ecadf199675edd9faf777e0b6a592054415804
giselle2323 <yakubuaminat94@gmail.com>
2020-08-31 23:55:49 +0100

    feat: questions menu tab on dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -4,14 +4,48 @@ import Question from '../components/Question'
 
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState('')
-  
+  const [menuTabs, setMenuTabs] = useState([{name: 'Unanswered'}, {name: 'Answered'}])
   const onChangeTab = () => {
 
   }
   
   return (
-    <div>Dashboard</div>
+    <>
+      <div>Dashboard</div>
+      <nav>
+        {menuTabs.map((tab) => (
+          <div
+            key={tab.name}
+            className={tab.name === activeTab
+            ? 'tab-link-item-active'
+            : 'tab-link-item'
+            }
+            onClick={onChangeTab(tab.name)}
+          >
+            {tab.name}
+          </div>
+        ))}
+      </nav>
+
+      <div>
+        
+      </div>
+    </>
   )
 }
 
-export default (connect)(Dashboard);
+const mapStateToProps = ({authedUser, questions, allUsers}) => {
+  const allQuestions = Object.keys(questions).sort(
+    (a,b) => questions[b].timestamp - questions[a].timestamp
+  );
+  const answeredQuestions = Object.keys(allUsers[authedUser].answers);
+  return {
+    authedUser: users[authedUser],
+    answeredQuestions,
+    unansweredQuestions: allQuestions.filter(
+      (e) => !answeredQuestions.includes(e)
+    )
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
