8ca26da8da5b8a5577923e04c00ce69964bb9b2f
giselle2323 <yakubuaminat94@gmail.com>
2020-09-09 23:51:48 +0100

    chore: resolved loading bar error

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { getInitialData } from '../redux/actions/shared'
+import { signOutUser } from '../redux/actions/users'
+import LoadingBar from 'react-redux-loading'
+import Nav from '../components/Nav'
 import SelectUser from '../components/SelectUser'
 import Dashboard from './Dashboard'
+
 const Home = (props) => {
   const { dispatch, users, authedUser} = props
   useEffect(() => {
@@ -10,12 +14,15 @@ const Home = (props) => {
   }, [dispatch]);
   console.log(users)
   return(
+    <>
+    <LoadingBar />
     <div>
-      {authedUser === null ? <SelectUser /> : <Dashboard />
+      <Nav authedUser={ authedUser } signOut={ signOutUser }/>
+      { authedUser === null ? <SelectUser /> : <Dashboard /> }
 
-      }
-        
     </div>
+    </>
+    
   )
 };
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
+import { loadingBarReducer } from "react-redux-loading"
 import { combineReducers } from 'redux'
 import authedUser from './users/authedUser'
 import users from './users/usersReducer'
 import questions from './questions/questionsReducer'
 
 export default combineReducers({
+  loadingBar: loadingBarReducer,
   authedUser,
   users,
   questions,
